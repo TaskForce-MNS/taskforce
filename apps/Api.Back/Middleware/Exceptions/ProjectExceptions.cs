3405c4b663fd8efa7fe2beda001f8f3f56d09bd8
6LLOYD <bayram.selim57@gmail.com>
2026-07-07 15:49:27 +0200

    bugfix(backend): fix exception code smells

--- apps/Api.Back/Middleware/Exceptions/ProjectExceptions.cs
+++ apps/Api.Back/Middleware/Exceptions/ProjectExceptions.cs
@@ -1,3 +1,21 @@
-// Middleware/Exceptions/ProjectExceptions.cs
-public class ProjectNotFoundException : Exception { public ProjectNotFoundException() : base("Projet introuvable.") { } }
-public class ProjectForbiddenException : Exception { public ProjectForbiddenException() : base("Accès interdit à ce projet.") { } }
+using System;
+
+namespace Api.Back.Middleware.Exceptions;
+
+public class ProjectNotFoundException : Exception
+{
+    public ProjectNotFoundException() : base("Projet introuvable.") { }
+
+    public ProjectNotFoundException(string message) : base(message) { }
+
+    public ProjectNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+public class ProjectForbiddenException : Exception
+{
+    public ProjectForbiddenException() : base("Accès interdit à ce projet.") { }
+
+    public ProjectForbiddenException(string message) : base(message) { }
+
+    public ProjectForbiddenException(string message, Exception innerException) : base(message, innerException) { }
+}
